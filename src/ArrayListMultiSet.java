3d727dca4223e168976dc6aaf4e9ab1bad807ef5
Pratyush Dwivedi <7183103-pratzyushd@users.noreply.gitlab.com>
2023-09-20 16:09:18 -0400

    Complete ArrayListMultiSet class

---
+++ src/ArrayListMultiSet.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class ArrayListMultiSet extends MultiSet {
+    private ArrayList<Object> list;
+
+    public ArrayListMultiSet() {
+        this.list = new ArrayList<Object>();
+    }
+
+    @Override
+    public boolean add(Object item) {
+        this.list.add(item);
+        return true;
+    }
+
+    @Override
+    public void remove(Object item) {
+        this.list.remove(item);
+    }
+
+    @Override
+    public boolean contains(Object item) {
+        return this.list.contains(item);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.list.isEmpty();
+    }
+
+    @Override
+    public int count(Object item) {
+        int count = 0;
+        for (Object el : this.list) {
+            if (el.equals(item)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public int size() {
+        return this.list.size();
+    }
+}
