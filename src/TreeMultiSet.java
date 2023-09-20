e05c739eb5b96797fc8c14bed632784cb9f6a028
Pratyush Dwivedi <7183103-pratzyushd@users.noreply.gitlab.com>
2023-09-20 15:58:51 -0400

    Complete TreeMultiSet class

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -1,2 +1,50 @@
-public class TreeMultiSet {
+public class TreeMultiSet implements MultiSet {
+    private final Tree tree;
+
+    public TreeMultiSet() {
+        this.tree = new Tree();
+    }
+
+    @Override
+    public boolean add(Object item) {
+        if (!(item instanceof Integer)) {
+            throw new IllegalArgumentException("TreeMultiSet only accepts Integers.");
+        }
+        this.tree.insert((Integer) item);
+        return true;
+    }
+
+    @Override
+    public void remove(Object item) {
+        if (!(item instanceof Integer)) {
+            throw new IllegalArgumentException("TreeMultiSet only accepts Integers.");
+        }
+        this.tree.deleteItem((Integer) item);
+    }
+
+    @Override
+    public boolean contains(Object item) {
+        if (!(item instanceof Integer)) {
+            throw new IllegalArgumentException("TreeMultiSet only accepts Integers.");
+        }
+        return this.tree.contains((Integer) item);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.tree.isEmpty();
+    }
+
+    @Override
+    public int count(Object item) {
+        if (!(item instanceof Integer)) {
+            throw new IllegalArgumentException("TreeMultiSet only accepts Integers.");
+        }
+        returh this.tree.count((Integer) item);
+    }
+
+    @Override
+    public int size() {
+        return this.tree.size();
+    }
 }
